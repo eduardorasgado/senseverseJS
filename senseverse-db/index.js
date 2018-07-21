846d02dd226f38ddca7b0c69e1901332c6308b1e
lal <eduardo.rasgado@hotmail.com>
2018-07-21 03:24:44 -0500

    index.js updated

--- senseverse-db/index.js
+++ senseverse-db/index.js
@@ -4,19 +4,21 @@ const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
 
-module.exports = function (config) {
-	const sequelize = setupDatabase(config)
-	const AgentModel = setupAgentModel(config)
-	const MetricModel = setupMetricModel(config)
-	//Definiendo relaciones
-	AgentModel.hasMany(MetricModel)
-	MetricModel.belongsTo(AgentModel)
-	//promesa para autenticar resuelta con async await
-	await sequielize.authenticate()
-	const Agent = {}
-	const Metric = {}
-	return {
-		Agent,
-		Metric
-	}
+module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+  // Definiendo relaciones
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+  // promesa para autenticar resuelta con async await
+  await sequelize.authenticate()
+  // crea bases
+  // sequielize.sync()
+  const Agent = {}
+  const Metric = {}
+  return {
+    Agent,
+    Metric
+  }
 }
