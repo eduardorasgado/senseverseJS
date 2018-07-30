44e0ef447dff173a127664750f94dd7351827c0b
lal <eduardo.rasgado@hotmail.com>
2018-07-30 18:56:06 -0500

    code coverage testing con nyc

--- senseverse-db/tests/agent-tests.js
+++ senseverse-db/tests/agent-tests.js
@@ -18,3 +18,20 @@ test('Agent', t => {
   // dice que existe un valor que resuelve a verdadero
   t.truthy(db.Agent, 'Agent service should exist')
 })
+/*
+Test Coverage
+Se encarga de checar si los test estan considerando todo el proyecto o no,
+por porcentajes por ejemplo, herramientas:
+
+istanbul
+nyc ->cli para istanbul
+
+instalacion:
+
+npm i nyc --save-dev
+
+Agregarlo asi en package json en scripts:
+	"test": "DEBUG=senseverse:* nyc --reporter=lcov ava tests/ --verbose"
+Para poder generar reportes y mandarlo a html
+Ver estos html en coverage, carpeta que se crea para reportear
+*/
