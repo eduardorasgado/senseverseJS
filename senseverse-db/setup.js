6ed7e008c78e11eb4e6b7c99a961f0f82de878dd
lal <eduardo.rasgado@hotmail.com>
2018-07-30 15:03:06 -0500

    error handling in setup/ db

--- senseverse-db/setup.js
+++ senseverse-db/setup.js
@@ -2,9 +2,34 @@
 // npm i debug --save
 // con ello podemos ver el log siemmpre y cuando tengamos variables de entorno
 // lo segundo es un name space proyecto-modulo-archivo
+/*
+Antes de correr activar la database:
+sudo systemctl start postgresql
+
+Despues para corregir bajo estandar:
+npm run lint
+Para corregir errores:
+npm run lint -- --fix
+
+*/
 const debug = require('debug')('senseverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
+const prompt = inquirer.createPromptModule()
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'senseverse',
     username: process.env.DB_USER || 'cheetos',
@@ -12,14 +37,15 @@ async function setup () {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: s => debug(s),
-    setup: true
+    setup: true,
+    operatorsAliases: false
   }
   // ejecutar, en caso de error, atrapamos
   await db(config).catch(handleFatalError)
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   // matamos el proceso
   process.exit(1)
