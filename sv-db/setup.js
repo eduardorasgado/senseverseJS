8d8a3d8dc681de5b2aad1d2bde8229c7f26fa0a0
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-30 14:47:43 -0600

    DB MODULE: Poka yoke to aviod destroying db. chalk and inquirer. Error handling in setup

--- sv-db/setup.js
+++ sv-db/setup.js
@@ -10,9 +10,27 @@ const Sequelize = require("sequelize");
 
 // debug package to script and db process monitoring
 // second parenthesis: a namespace -> project:module:file
-const debug = require('debug')('senseverse:db:setup');
+const debug = require("debug")('senseverse:db:setup');
+// colors in console
+const chalk = require("chalk");
+// to make user some questions
+const inquirer= require("inquirer");
+
+// promt let me mkae questions to user, those questions are promises
+const prompt = inquirer.createPromptModule();
 
 async function setup() {
+    // before all we make a question
+    const answer = await prompt([
+        {
+            type: 'confirm',
+            name: 'setup',
+            message: 'This is going to destroy DB, are you sure?'
+        }
+    ]);
+    // handling answer, in case of negative answer, just exit
+    if(!answer.setup) return console.log('Operation cancelled');
+
     const config = {
         database: process.env.DB_NAME || 'senseversedb',
         username: process.env.DB_USER || 'senseversedb',
