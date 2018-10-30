e4a8e6c823e82faf007d30cf07d8fee4b6783460
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-30 15:43:41 -0600

    DB MODULE: getting started with unit testing by installig AVA module and initializing our first test to check installation.

---
+++ sv-db/tests/agent-tests.js
@@ -0,0 +1,14 @@
+/*
+* UNIT TESTING FOR TEST DRIVEN SOFTWARE DEVELOPMENT
+* */
+
+'use strict';
+/* requiring ava, a Unit Testing enviroment for NodeJS*/
+const test = require('ava');
+
+// check package.json to test script setting
+test('make it pass', t =>
+{
+    // this is a test for ava installed checking
+    t.pass()
+});
