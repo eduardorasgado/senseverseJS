f804557b92d5eb6b71b8c9087aa34a962ec4871b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-30 13:55:11 -0600

    DB MODULE: importing models and iinitializing them in index

--- sv-db/index.js
+++ sv-db/index.js
@@ -1,13 +1,37 @@
 'use strict';
 
+const setupDatabase = require("./lib/db");
+const setupAgentModel = require("./models/agent");
+const setupMetricModel = require("./models/metric");
+
 // this module should return a promise
-module.exports = function(config)
+module.exports = async function(config)
 {
+    // this design pattern will let us create
+    // easy unit testing
+    const sequelize = setupDatabase(config);
+    const AgentModel = setupAgentModel(config);
+    const MetricModel = setupMetricModel(config);
+
+    // defining relations
+    // creating foreign keys between models
+    AgentModel.hasMany(MetricModel);
+    MetricModel.belongsTo(AgentModel);
+
+    // validating if db is well initialized
+    // solving by Promise
+    // if this function fails, the general function
+    // will stop
+    await sequelize.authenticate();
+
+    // database configuration
+    //sequelize.sync();
+
     const Agent = {};
     const Metric = {};
 
     return {
         Agent,
         Metric
-    }
+    };
 };
--- sv-db/lib/db.js
+++ sv-db/lib/db.js
@@ -5,6 +5,7 @@ const Sequelize = require('sequelize');
 
 let sequelize = null;
 
+// just return an instance of sequelize
 module.exports = function setupDatabase (config)
 {
     // in case sequelize does not exist it should be
