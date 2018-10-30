37184bed5835412ca72a9eb33865cd932f290e12
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-30 13:11:41 -0600

    DB MODULE: db module basic structure

---
+++ sv-db/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// this module should return a promise
+module.exports = function(config)
+{
+    const Agent = {};
+    const Metric = {};
+
+    return {
+        Agent,
+        Metric
+    }
+};
