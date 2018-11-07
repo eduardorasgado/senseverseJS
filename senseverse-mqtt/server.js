9cfad519af6c38150f9f0e4d88c4ba5eceb20a77
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-06 22:28:49 -0600

    MQTT MODULE: server services handles connections, payloads.

--- senseverse-mqtt/server.js
+++ senseverse-mqtt/server.js
@@ -21,6 +21,26 @@ const settings = {
 // event emitter
 const server = new mosca.Server(settings);
 
+server.on('clientConnected', client =>
+{
+    // id is automatically created by mosca server
+    debug(`${chalk.red.bold("Client Connected")}: ${client.id} `);
+});
+
+server.on('clientDisconnected', client =>
+{
+    debug(`${chalk.red.bold("Client disconnected:")} ${client.id}`);
+});
+
+// what is the packet and who sent it
+server.on('published', (packet, client) =>
+{
+    // topic is message type
+    debug(`Received: ${packet.topic}`);
+    // payload is where pkg info is stored
+    debug(`[Payload]: ${chalk.green.bold(packet.payload)}`);
+});
+
 server.on('ready', () =>
 {
     console.log(`${chalk.green.bold('[platziverse-mqtt]')} server is running.`);
