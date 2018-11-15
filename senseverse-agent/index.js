83c954c541df3ae4f6c62e69f4b76805a455b1a5
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-15 07:22:58 -0600

    AGENT MODULE: agent emitter connect and disconnect methods created and tested.

--- senseverse-agent/index.js
+++ senseverse-agent/index.js
@@ -5,12 +5,43 @@ const EventEmitter =  require("events");
 
 class SenseverseAgent extends EventEmitter
 {
-    //
-    constructor()
-    {
+    // options come at the moment class is called
+    constructor(opts) {
         //calling constructor from extended class
         super();
+
+        // internal reference to options
+        this._options = opts;
+        // flag to know if timer has been initialized
+        this._started = false;
+        // interval for sending and receiving data frequency
+        this._timer = null;
     }
+        connect()
+        {
+
+            if (!this._started)
+            {
+                this._started = true;
+                const opts = this._options;
+                // notifying the agent is connected
+                this.emit('connected');
+                this._timer = setInterval(() =>
+                {
+                    // callback each interval seconds
+                    this.emit('agent/message', 'this is a message');
+                }, opts.interval);
+            }
+        }
+
+        disconnect()
+        {
+            if (this._started) {
+                clearInterval(this._timer);
+                this._started = false;
+                this.emit('disconnected');
+            }
+        }
 }
 
 // can be call as:
